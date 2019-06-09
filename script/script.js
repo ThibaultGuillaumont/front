12fde3fcc91b0656b01f4171959892d02d730883
Thibault <t.guillaumont@holusion.com>
2019-06-09 10:56:31 +0200

    parralax background

--- script/script.js
+++ script/script.js
@@ -1,16 +1,20 @@
 var background_1 = new Image();
 var background_2 = new Image();
+var background_far_1 = new Image();
 var speed_back1 = 5;
+var speed_back_far = 3.5;
 var title_opacity = 1;
 var startTime = new Date();
+var background_position = 1080 - 400;
+var background_far_position = background_position -200;
 
 title_flag = true;
 function init(){
 
   background_1.src = 'img/background-tilable.png';
   background_2.src = 'img/background-tilable.png';
-  console.log(background_2);
-  background_position = 1080 - 400;
+  background_far_1.src = "img/background-far-tilable.png"
+
   window.requestAnimationFrame(draw);
 }
 
@@ -26,25 +30,15 @@ function draw() {
 
   ctx.save();
 
-  //DRAW BACKGROUND
-  var time = new Date();
-  position1 = 2373*(elapsed/100000)*speed_back1 % 2373;
-  ctx.translate(position1,background_position);
-  ctx.drawImage(background_1,0,0);
-  ctx.restore();
-  ctx.save();
-  //make tilable
-  position2 = (2373*(elapsed/100000)*speed_back1 % 2373 ) -2373;
-  ctx.translate(position2,background_position);
-  ctx.drawImage(background_2,0,0);
-  ctx.restore();
-  ctx.save();
+  draw_background(elapsed);
 
   //DEBUG
   ctx.font = "30px Arial";
   ctx.fillStyle = "grey";
   ctx.fillText(position2,500,50);
   ctx.restore();
+
+
 
   if (title_flag) {
     setTimeout(function(){
@@ -56,6 +50,39 @@ function draw() {
   title_opacity = title_opacity-(elapsed/30000);
 
   window.requestAnimationFrame(draw);
+}
+
+function draw_background(elapsed) {
+    var ctx = document.getElementById('canvas').getContext('2d');
+  //DRAW BACKGROUND
+  position1 = 2373*(elapsed/100000)*speed_back1 % 2373;
+  ctx.translate(position1,background_position);
+  ctx.drawImage(background_1,0,0);
+  ctx.restore();
+  ctx.save();
+  //make tilable
+  position2 = (2373*(elapsed/100000)*speed_back1 % 2373 ) -2373;
+  ctx.translate(position2,background_position);
+  ctx.drawImage(background_2,0,0);
+  ctx.restore();
+  ctx.save();
+  console.log(elapsed);
+
+
+  //DRAW Far AWAY BACKGROUND
+  position1 = 2772*(elapsed/100000)*speed_back_far % 2772;
+  ctx.translate(position1,background_far_position);
+  ctx.drawImage(background_far_1,0,0);
+  ctx.restore();
+  ctx.save();
+  //make tilable
+  position2 = (2772*(elapsed/100000)*speed_back_far % 2772 ) -2772;
+  ctx.translate(position2,background_far_position);
+  ctx.drawImage(background_far_1,0,0);
+  ctx.restore();
+  ctx.save();
+
+
 }
 
 init();
